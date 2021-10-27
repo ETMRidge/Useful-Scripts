acf7e7035f267a6cf7d344ff29da932b8d89447f
ETMRidge <92232253+ETMRidge@users.noreply.github.com>
2021-10-27 16:28:45 +0100

    Update abuseipdb.sh

--- abuseipdb.sh
+++ abuseipdb.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-ip_list=( $( last -10 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )
+ip_list=( $( last | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )
 
 for x in "${ip_list[@]}"; do
+echo "IP Address: "$x""
 curl -G https://api.abuseipdb.com/api/v2/check -s \
   --data-urlencode """ipAddress=$x""" \
   -d maxAgeInDays=90 \
   -d verbose \
   -d confidenceMinimum=0 \
-  -H "Key: $API" \
+  -H "Key: 5df31c46e180154cce403cbbcfb73f32efac327bb946e5b3b72d562064b75ac22f05b37e3b8088ba" \
   -H "Accept: application/json" | grep -o -w -E '"abuseConfidenceScore":[0-9]*'
-echo "$x"
+curl -s --request GET \
+     --url https://api.greynoise.io/v3/community/"$x" \
+     --header 'Accept: application/json' \
+     --header 'key: S8axYtuPmhme47Hl9IvyeWS67LDON0QPiuwhndmPul9Zo4c3Cq2itu9HdbZbSzHI' | grep -o -w -E '"riot": [a-zA-Z]+,'
+echo ""
 done
