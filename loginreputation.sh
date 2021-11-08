f11c91afc30799cc94aba6372bf3eb2d4e27d4b6
ETMRidge <92232253+ETMRidge@users.noreply.github.com>
2021-11-08 15:33:32 +0000

    Update loginreputation.sh

--- loginreputation.sh
+++ loginreputation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ip_list=( $( last -5 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )
-rule_it_out_number=( $(grep -Ei '"riot": true,' output.txt | wc -l ) )
-rule_it_out_list=($(grep -B 5 '"riot": true,' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )
-potential_malicious_ip=( $( grep -B 5 '"abuseConfidenceScore":^(?:[1-9]|\d\d\d*)$' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )
-potential_mal_number=( $(grep -Ei '"abuseConfidenceScore":^(?:[1-9]|\d\d\d*)$' output.txt | wc -l ) )
+ip_list=( $( last -5 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )  #Edit the command where last -5 is.  It will work on files using cat.
+
+rule_it_out_number=( $(grep -Ei '"riot": true,' output.txt | wc -l ) ) #Grepping for riot and counting how many can be ruled out.
+rule_it_out_list=($(grep -B 5 '"riot": true,' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) ) #Gathering a list of RIOT IP's
+potential_malicious_ip=( $( grep -B 5 '"abuseConfidenceScore":^(?:[1-9]|\d\d\d*)$' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) ) #Gathering a list of IP's with abuseipdb scores over >=1
+potential_mal_number=( $(grep -Ei '"abuseConfidenceScore":^(?:[1-9]|\d\d\d*)$' output.txt | wc -l ) ) #Getting the number of IP's with abuseipdb scores over >=1
 
 {
 for x in "${ip_list[@]}"; do
@@ -22,16 +23,16 @@ curl -s --request GET \
      --header 'key: S8axYtuPmhme47Hl9IvyeWS67LDON0QPiuwhndmPul9Zo4c3Cq2itu9HdbZbSzHI' | grep -o -w -E -e '"riot": [a-zA-Z]+,' -e '"noise": [a-zA-Z]+,'
 echo ""
 done
-} | tee output.txt
+} | tee output.txt #Taking all output from the above greps and putting it into output.txt
 
 echo "Rule out the following $rule_it_out_number IP's:"
 
-for x in "${rule_it_out_list[@]}"; do
+for x in "${rule_it_out_list[@]}"; do #For every IP in the RIOT list echo it.
 echo "$x"
 done
 
 echo "The following $potential_mal_number IP's have an abuse score above 0 and should be investigated:"
 
-for x in "${potential_malicious_ip[@]}"; do
+for x in "${potential_malicious_ip[@]}"; do #For every IP in the malicious IP list echo it
 echo "$x"
 done
