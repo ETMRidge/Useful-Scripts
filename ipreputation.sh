47d1057064f28349669c79b44a8b980025bde508
ETMRidge <92232253+ETMRidge@users.noreply.github.com>
2021-11-12 13:32:07 +0000

    Update and rename loginreputation.sh to ipreputation.sh
    This change adds metrics which actually fucntion now.

--- ipreputation.sh
+++ ipreputation.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-ip_list=( $( last -5 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) )  #Edit the command where last -5 is.  It will work on files using cat.
-
-rule_it_out_number=( $(grep -Ei '"riot": true,' output.txt | wc -l ) ) #Grepping for riot and counting how many can be ruled out.
-rule_it_out_list=($(grep -B 5 '"riot": true,' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) ) #Gathering a list of RIOT IP's
-potential_malicious_ip=( $( grep -B 5 '"abuseConfidenceScore":^(?:[1-9]|\d\d\d*)$' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" ) ) #Gathering a list of IP's with abuseipdb scores over >=1
-potential_mal_number=( $(grep -Ei '"abuseConfidenceScore":^(?:[1-9]|\d\d\d*)$' output.txt | wc -l ) ) #Getting the number of IP's with abuseipdb scores over >=1
+ip_list=( $( cat filename | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u ) )  #Edit the command where last -5 is.  It will work on files using cat.
 
 {
 for x in "${ip_list[@]}"; do
@@ -23,7 +18,16 @@ curl -s --request GET \
      --header 'key: S8axYtuPmhme47Hl9IvyeWS67LDON0QPiuwhndmPul9Zo4c3Cq2itu9HdbZbSzHI' | grep -o -w -E -e '"riot": [a-zA-Z]+,' -e '"noise": [a-zA-Z]+,'
 echo ""
 done
-} | tee output.txt #Taking all output from the above greps and putting it into output.txt
+} | tee output.txt  #Taking all output from the above greps and putting it into output.txt
+
+rule_it_out_number=( $(grep -Ei '"riot": true,' output.txt | wc -l ) ) #Grepping for riot and counting how many can be ruled out.
+rule_it_out_list=($(grep -B 5 '"riot": true,' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u ) ) #Gathering a list of RIOT IP's
+potential_malicious_ip=( $( grep -E -B 5 '"abuseConfidenceScore":[1-9]+' output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u ) ) #Gathering a list of IP's with abuseipdb scores over >=1
+potential_mal_number=( $( grep -E '"abuseConfidenceScore":[1-9]+' output.txt | wc -l ) ) #Getting the number of IP's with abuseipdb scores over >=1
+totalip=( $(cat output.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l) )
+
+echo "Total number of IP's:"
+echo "$totalip"
 
 echo "Rule out the following $rule_it_out_number IP's:"
 
