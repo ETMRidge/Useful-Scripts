032e7dd085914871f1a62a53907a9b7155509ff7
ETMRidge <92232253+ETMRidge@users.noreply.github.com>
2022-04-28 11:28:07 +0100

    Update virustotal-hashrep.sh

--- virustotal-hashrep.sh
+++ virustotal-hashrep.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-hash_list=( $(cat hash.txt) ) #| grep -Ei '*\n' | sort -u ) )
+hash_list=( $(cat hash.txt) # This by default will go line by line as it's a list.
 
 for x in "${hash_list[@]}"; do
+echo "" # Space to make it readable.
 echo "hash: "$x""
 
 curl --request GET \
   --url https://www.virustotal.com/api/v3/search?query="$x" \
-  --header 'x-apikey: 0b37182ad661937789a806373b76984c379c9f061c48bb59e96f6e108cfb672a'
+  --header 'x-apikey: ()' #API key goes here, remove the brackets once entered.
+
+sleep 15 #Sleeps for 15 seconds to ensure it efficiently meets the API limit.
 done
