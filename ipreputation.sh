5b1a98a18fce5f2bfbc51ca228b0a183a999e821
ETMRidge <92232253+ETMRidge@users.noreply.github.com>
2023-03-31 10:39:13 +0100

    Update ipreputation.sh

--- ipreputation.sh
+++ ipreputation.sh
@@ -10,7 +10,7 @@ curl -G https://api.abuseipdb.com/api/v2/check -s \
   -d maxAgeInDays=90 \
   -d verbose \
   -d confidenceMinimum=0 \
-  -H "Key: 5df31c46e180154cce403cbbcfb73f32efac327bb946e5b3b72d562064b75ac22f05b37e3b8088ba" \
+  -H "Key: " \
   -H "Accept: application/json" | grep -o -w -E -e '"countryName":"([a-zA-Z]+( [a-zA-Z]+)+)",' -e '"countryName":"[a-zA-Z]+",' -e '"abuseConfidenceScore":[0-9]**'
 curl -s --request GET \
      --url https://api.greynoise.io/v3/community/"$x" \
